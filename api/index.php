e5f2943bb42eff296b7dba947c1f56e6c3de80cc
NaroX007 <ariqhidayat101@gmail.com>
2025-12-23 01:34:22 +0700

    nineteenth commit

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+@mkdir('/tmp/storage/framework/cache', 0777, true);
+@mkdir('/tmp/storage/framework/sessions', 0777, true);
+@mkdir('/tmp/storage/framework/views', 0777, true);
+
+require __DIR__ . '/../public/index.php';
